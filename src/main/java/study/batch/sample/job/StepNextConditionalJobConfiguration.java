6e86a9cea436c79b4fe4bf588624320814d6db67
bca <techinha221@naver.com>
2021-04-24 18:22:08 +0900

    chapter3 -2 step flow -on, to, end

---
+++ src/main/java/study/batch/sample/job/StepNextConditionalJobConfiguration.java
@@ -0,0 +1,79 @@
+package study.batch.sample.job;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+public class StepNextConditionalJobConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    /**
+     *   step 1 실패 시, step1 -> step3
+     *   step 1 성공 시, step1 -> step2 -> step3
+     */
+
+    @Bean
+    public Job stepNextConditionalJob() {
+        return jobBuilderFactory.get("stepNextConditionalJob")
+                .start(conditionalJobStep1())
+                    .on("FAILED") //FAILED 경우
+                    .to(conditionalJobStep3()) //step 3 이
+                    .on("*")//Step3 결과와 상관없이
+                    .end() //Step 3으로 이동하면 종료
+                .from(conditionalJobStep1()) //step1 로 부터, step1의 이벤트 캐치가 FAILED로 되있는 상태에서 추가로 이벤트 캐치하려면 from을 써야만 함
+                    .on("*") // FAILED 외에 모든 경우
+                    .to(conditionalJobStep2())
+                    .next(conditionalJobStep3())
+                    .on("*")
+                    .end()
+                .end()
+                .build();
+    }
+
+    @Bean
+    public Step conditionalJobStep1() {
+        return stepBuilderFactory.get("conditionalJobStep1")
+                .tasklet((contribution, chunkContext) ->
+                {
+                    log.info(">>>>> This is stepNextConditionalJob Step1");
+                 //   contribution.setExitStatus(ExitStatus.FAILED);
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public Step conditionalJobStep2() {
+        return stepBuilderFactory.get("conditionalJobStep2")
+                .tasklet((contribution, chunkContext) ->
+                {
+                    log.info(">>>>> This is stepNextConditionalJob Step2");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public Step conditionalJobStep3() {
+        return stepBuilderFactory.get("conditionalJobStep3")
+                .tasklet((contribution, chunkContext) ->
+                {
+                    log.info(">>>>> This is stepNextConditionalJob Step3");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+}
